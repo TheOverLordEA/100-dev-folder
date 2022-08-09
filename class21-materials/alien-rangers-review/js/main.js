5542f8870ef923a7f029f32ce23874a170f7bd37
Erkhembayar Atarbold <74484951+TheOverLordEA@users.noreply.github.com>
2022-08-09 02:31:31 -0700

    added new stuff to class21

--- class21-materials/alien-rangers-review/js/main.js
+++ class21-materials/alien-rangers-review/js/main.js
@@ -1,9 +1,25 @@
 //Arrays
-
+let arra = [];
 //Create and array of tv shows. Loop through and print each show to the console
+arra = ["archer", "shee", "bear"];
+for (let a of arra) {
+  //   console.log(a);
+}
 
 //Create and array of numbers
+const arraa = [1, 100, 10000, 11, 1000000];
 //Return a new array of numbers that includes every even number from the previous Arrays
+for (let e of arraa) {
+  if (e % 2 === 0) {
+    // console.log(e);
+  }
+}
 
 //Create a function that takes in an array of numbers
+function morty(arra) {
+  const newArr = arra.sort((a, b) => a - b);
+  console.log(newArr[1], newArr[newArr.length - 2]);
+}
+
+// console.log(morty([10, 1, 88, 100, 10001]));
 //Alert the sum of the second lowest and the second highest number
--- class21-materials/objects-zeo/js/main.js
+++ class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,21 @@
 //Create a stopwatch object that has four properties and three methods
+const watchBe = {
+  quality: "good",
+  age: 2018,
+  timeMinute: 10,
+  timeSec: 100,
+
+  getTime: function (timeMin, timeSec) {
+    return `${this.timeMinute} ${this.timeSec}`;
+  },
+
+  getAge: function (age) {
+    return this.age;
+  },
+
+  presentQuality: function (a) {
+    return this.quality;
+  },
+};
+
+// console.log(watchBe.presentQuality());
