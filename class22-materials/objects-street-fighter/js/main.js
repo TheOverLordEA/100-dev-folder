a3047acf0a71537385b37eb9b92ef4d7a1d2e904
Erkhembayar Atarbold <74484951+TheOverLordEA@users.noreply.github.com>
2022-08-16 01:58:13 -0700

    add

--- class22-materials/objects-street-fighter/js/main.js
+++ class22-materials/objects-street-fighter/js/main.js
@@ -1,1 +1,23 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+function CreateAi(power, speed, fat, health, punch, block, healthIncrease) {
+  this.power = power;
+  this.speed = speed;
+  this.fat = fat;
+  this.health = health;
+
+  this.block = function () {
+    console.log(`${power}`);
+  };
+
+  this.punch = function () {
+    console.log(`${power}`);
+  };
+
+  this.healthIncrease = function () {
+    console.log(`${healthIncrease}`);
+  };
+}
+
+// const lione = new CreateAi("alien", "fast", true, 100, "punch", "block", "hea");
+
+// console.log(lione.block());
