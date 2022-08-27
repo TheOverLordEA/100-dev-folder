40ff1b8db82d62c27ee784f955ef5ba0d29fe8ee
Erkhembayar Atarbold <74484951+TheOverLordEA@users.noreply.github.com>
2022-08-26 23:56:57 -0700

    Built a bento data structure

--- class26-materials/objects-lost-galaxy/js/main.js
+++ class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,22 @@
-//Create a dog object that has four properties and three methods
+//Create a bento object that has four properties and three methods
+
+const bento = {};
+
+bento.rice = "white";
+
+bento.sauce = "chilly";
+
+bento.vegtable = "oniion";
+bento.tofu = "allow";
+
+bento.fryTofu = function () {
+  this.tofu = "smoked";
+};
+
+bento.addSauce = function () {
+  this.sauce += "lavaa";
+};
+
+bento.changeGreens = function () {
+  this.vegtable = "gokus";
+};
--- war-code/kyu6/main.js
+++ war-code/kyu6/main.js
@@ -1,11 +1,23 @@
 //A tribnoichi funtion
 
-
-
 const tribonichii = (array) => {
-    //Loop thorugh array
+  //Loop thorugh array
+  //Count the last three ele in the array
+  //
+};
 
-    //Count the last three ele in the array
-
-    //
+//Break camelcase progrma
+function a(st) {
+  let str = "";
+  for (let i = 0; i < st.length; i++) {
+    if (st[i] === st[i].toUpperCase()) {
+      str += " ";
+      str += st[i];
+    } else {
+      str += st[i];
+    }
+  }
+  return str;
 }
+
+console.log(a("camelCasingTest"));
