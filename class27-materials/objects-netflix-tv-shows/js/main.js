1ff427dd38ec21f79eea4e3d102c6d9cf027daad
Erkhembayar Atarbold <74484951+TheOverLordEA@users.noreply.github.com>
2022-08-31 00:02:42 -0700

    Add file 28

--- class27-materials/objects-netflix-tv-shows/js/main.js
+++ class27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,13 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
+class NetFlix {
+  constructor(movie, brandNew, old) {
+    this.movie = movie;
+    this.brandNew = brandNew;
+    this.old = old;
+  }
 
+  findMovie() {
+    console.log(`${this.movie}`);
+  }
+}
