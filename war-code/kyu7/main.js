196512f087a8a69797ec6ca403eab85cdf95abfa
Erkhembayar Atarbold <74484951+TheOverLordEA@users.noreply.github.com>
2022-09-23 22:30:48 -0700

    Class 29 file included

--- war-code/kyu7/main.js
+++ war-code/kyu7/main.js
@@ -147,3 +147,46 @@ function multiple(str) {
 //Write function to sum kyu
 
 function series(a) {}
+
+//Find the shortest array
+function shortestArray(arr) {
+  let string = arr.split(" ");
+  let newArr = [];
+  console.log(string);
+
+  const a = string.sort((a, b) => a.length - b.length);
+  console.log(a);
+  return a[0].length;
+}
+
+console.log(
+  shortestArray("bitcoin take over the world maybe who knows perhaps")
+);
+
+// const data = [
+//   "car",
+//   "car",
+//   "truck",
+//   "truck",
+//   "bike",
+//   "walk",
+//   "car",
+//   "van",
+//   "bike",
+//   "walk",
+//   "car",
+//   "van",
+//   "car",
+//   "truck",
+//   "pogostick",
+// ];
+
+// const transportation = data.reduce(function (obj, item) {
+//   if (!obj[item]) {
+//     obj[item] = 0;
+//   }
+//   obj[item]++;
+//   return obj;
+// }, {});
+
+// console.log(transportation);
