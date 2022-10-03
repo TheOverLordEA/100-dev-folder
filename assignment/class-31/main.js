4c2bfb937ffe9841bf2751ef063827f8d540a2c6
Erkhembayar Atarbold <74484951+TheOverLordEA@users.noreply.github.com>
2022-10-03 05:39:53 -0700

    Built a new class 31

---
+++ assignment/class-31/main.js
@@ -0,0 +1,23 @@
+class Game {
+  constructor(el) {
+    this._el = document.querySelector(el);
+  }
+  get el() {
+    return this.el;
+  }
+}
+
+class ButtonFirst extends Game {
+  constructor(el, value) {
+    super(el);
+    this._value = value;
+  }
+  get value() {
+    return this.value;
+  }
+  newLetter() {
+    return (el.textContent = value);
+  }
+}
+
+const button = new ButtonFirst("div", "O");
