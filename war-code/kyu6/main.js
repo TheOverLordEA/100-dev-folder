344f9ad1d34d577693b62b50409fbcb27d8a7d20
Erkhembayar Atarbold <74484951+TheOverLordEA@users.noreply.github.com>
2022-10-19 18:53:55 +0800

    New kata created

--- war-code/kyu6/main.js
+++ war-code/kyu6/main.js
@@ -47,3 +47,21 @@ function findPath(array) {
 }
 
 // console.log(findPath(["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"]));
+
+function gooseFilter(birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+
+  return birds.filter((e) => !geese.includes(e));
+  // return an array containing all of the strings in the input array except those that match strings in geese
+}
+console.log(
+  gooseFilter([
+    "Mallard",
+    "Hook Bill",
+    "African",
+    "Crested",
+    "Pilgrim",
+    "Toulouse",
+    "Blue Swedish",
+  ])
+);
